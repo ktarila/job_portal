da61018dd3b9c4fdf02a9e5c7c78d70cae5cdc00
Patrick Kenekayoro <ktarila@gmail.com>
2020-10-05 09:34:11 +0100

    upgraded symfony

--- config/bundles.php
+++ config/bundles.php
@@ -20,4 +20,5 @@ return [
     Lexik\Bundle\JWTAuthenticationBundle\LexikJWTAuthenticationBundle::class => ['all' => true],
     Gesdinet\JWTRefreshTokenBundle\GesdinetJWTRefreshTokenBundle::class => ['all' => true],
     Vich\UploaderBundle\VichUploaderBundle::class => ['all' => true],
+    Symfony\Bundle\WebProfilerBundle\WebProfilerBundle::class => ['dev' => true, 'test' => true],
 ];
